8edd0170423537a650c51adac6fbae8249080741
wptaskmail-stack <wptaskmail@gmail.com>
2026-06-20 04:13:06 +0600

    news
    new

--- src/app/(payload)/api/graphql/route.ts
+++ src/app/(payload)/api/graphql/route.ts
@@ -1,5 +1,5 @@
 import config from '@payload-config'
-import { GRAPHQL_POST, GRAPHQL_OPTIONS } from '@payloadcms/next/routes'
+import { GRAPHQL_POST, REST_OPTIONS } from '@payloadcms/next/routes'
 
 export const POST = GRAPHQL_POST(config)
-export const OPTIONS = GRAPHQL_OPTIONS(config)
+export const OPTIONS = REST_OPTIONS(config)
